750d97fd84a7f5797aa57dc51aff4c29846a2a1b
beth <beth.gooding@hotmail.com>
2021-04-30 10:27:14 +0100

    adds pokemon model

--- pokegotchi/pages/models.py
+++ pokegotchi/pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200, null=True)
+    age = models.DecimalField(decimal_places=2, max_digits=10)
+    pokemon = models.CharField(max_length=200)
+    happiness = models.DecimalField(decimal_places=2, max_digits=10)
+    hunger = models.DecimalField(decimal_places=2, max_digits=10)
