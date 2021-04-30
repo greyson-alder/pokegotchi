1cfcff60b4b48c5f9deb46b3b4fe6d747c5e6e53
beth <beth.gooding@hotmail.com>
2021-04-30 10:06:13 +0100

    Sets up basic home url

--- pokegotchi/pages/views.py
+++ pokegotchi/pages/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+def index(req):
+    return HttpResponse("Pokegotchi!")
