daafd771523e599450f46620293b5d47c941382d
beth <beth.gooding@hotmail.com>
2021-05-04 18:45:10 +0100

    Adds pages folder

---
+++ frontend/pokegotchi/src/pages/About.js
@@ -0,0 +1,12 @@
+import Title from '../components/Title'
+
+const About = () => {
+    return (
+        <div>
+            <Title title="About"></Title>
+            <p>Some text about how this is a group project. Pokemon tamagotchi crossover. Our names</p>
+        </div>
+    )
+}
+
+export default About
